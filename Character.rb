f6910f475f252829bf60242348d6d66af67e5f96
Marcin Bratek <m.bratek@gmail.com>
2013-05-10 22:43:53 +0200

    Added some logic to character

--- Character.rb
+++ Character.rb
@@ -1,8 +1,38 @@
 class Character
-  attr_reader :name, :level
+  attr_reader :name, :level, :max_hp, :hp, :exp
 
   def initialize(name, level)
-    @name, @level = name, level
+    @name, @level, @hp, @max_hp, @exp = name, level, 100, 100, 0
+  end
+
+  def next_level?
+    return true if @exp >= @level * 100
+    return false
+  end
+
+  def next_level
+    if next_level?
+      @exp = 0
+      @level += 1
+    end
+  end
+
+  def next_level!
+    @exp = 0
+    @level += 1
+  end
+
+  def dead?
+    return true if @hp <= 0
+    return false
+  end
+
+  def heal hp
+    @hp += hp
+  end
+
+  def add_exp exp
+    @exp += exp
   end
 
 end
