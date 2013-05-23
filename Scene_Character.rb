f2c685ad2fdd2c77a373383fa920cb5e96e35f69
Marcin Bratek <m.bratek@gmail.com>
2013-05-23 20:45:58 +0200

    Added hp and exp values to show up

--- Scene_Character.rb
+++ Scene_Character.rb
@@ -10,6 +10,8 @@ class Scene_Character < Scene
         "Back" => lambda { @window.scene = Transition.new(@window, Scene_Map.new(@window, @config['map'], @config['map_graphic']), :in, false) }
         }, 0)
     @window_menu.active = true
+
+    @character = @window.character
   end
   
   
@@ -24,8 +26,8 @@ class Scene_Character < Scene
   def draw
     @window_menu.draw
     @window.font.draw("HP", @window.width-140, 0, 20)
-    @window_menu.drawHPBar(@window.width-100, 0, 100, 20, Color.new(255, 0, 255, 0), Color.new(255, 0, 100, 0), 10, 100)
+    @window_menu.drawHPBar(@window.width-100, 0, 100, 20, Color.new(255, 0, 255, 0), Color.new(255, 0, 100, 0), @character.hp, 100)
     @window.font.draw("EXP", @window.width-140, 25, 20)
-    @window_menu.drawEXPBar(@window.width-100, 25, 100, 20, Color.new(255, 0, 0, 255), Color.new(255, 0, 0, 100), 10, 100)
+    @window_menu.drawEXPBar(@window.width-100, 25, 100, 20, Color.new(255, 0, 0, 255), Color.new(255, 0, 0, 100), @character.exp, 100)
   end
 end
